bcdf331cb26173bfbfeae1b585440c1ae4926fd2
Alena Novoselova <alenka4510@gmail.com>
2019-06-02 23:34:36 +0700

    пофиксина ошибка воспроизведения и подсчета ответов на втором и третьем уровнях

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 ﻿var AMOUNT_TIME_ON_ANSWER_IN_SECONDS = 2;
-var CAMOUNT_TIME_FOR_TEST_IN_SECONDS = 6;
+var CAMOUNT_TIME_FOR_TEST_IN_SECONDS = 4;
 
 class Test {
-  constructor(options) {
+  constructor() {
     this.AMOUNT_TIME_FOR_TEST_IN_SECONDS = CAMOUNT_TIME_FOR_TEST_IN_SECONDS;
     this.KEYCODE_LEFT_BUTTON = 37;
     this.KEYCODE_RIGHT_BUTTON = 39;
@@ -20,24 +20,27 @@ class Test {
     this.amountTasks = 0;
     this.level = 0;
     this.nextTaskTimeout = undefined;
-    this.result = new Result(),
-    this.rightAnswersLevel1 = 0;
-    this.wrongAnswersLevel1 = 0;
-    this.rightAnswersLevel2_1 = 0;
-    this.wrongAnswersLevel2_1 = 0;
-    this.rightAnswersLevel2_2 = 0;
-    this.wrongAnswersLevel2_2 = 0;
-    this.amountTasksLevel1 = 0;
-    this.amountTasksLevel2_1 = 0;
-    this.amountTasksLevel2_2 = 0;
 	this.answersTimes;
 	this.taskStartTime = undefined;
     this.passedTasks = [];
     this.reactionTime = [];
     this.tasksInTest = [];
-
-	this.firstTime = true;
   };
+
+  newLevel()
+  {
+    //this.nextTaskTimeout = undefined;
+    this.answersTimes;
+    //this.taskStartTime = undefined;
+    this.leastTimeForTest = CAMOUNT_TIME_FOR_TEST_IN_SECONDS;
+    this.testIsEnded = false;
+    this.passedTasks = [];
+    this.reactionTime = [];
+    this.tasksInTest = [];
+    this.rightAnswers = 0;
+    this.wrongAnswers = 0;
+    this.amountTasks = 0;
+  }
 
   setLevel(newLevel) {
     this.level = newLevel;
@@ -45,13 +48,17 @@ class Test {
   };
 
   setIntervalForNextTask() {
-    this.nextTaskTimeout = setInterval(this.nextTask.bind(this), this.task.AMOUNT_TIME_FOR_TASK_IN_MILLISECONDS);
-    console.log({createdTimer: this.nextTaskTimeout});
+   // this.nextTaskTimeout = setInterval(this.nextTask.bind(this), this.task.AMOUNT_TIME_FOR_TASK_IN_MILLISECONDS);
+    clearTimeout(this.nextTaskTimeout);
+    console.log({"clearTimeout:" : this.nextTaskTimeout });
+    this.nextTaskTimeout = setTimeout(this.nextTask.bind(this), this.task.AMOUNT_TIME_FOR_TASK_IN_MILLISECONDS);
+    console.log({"createdTimer Из тextTask": this.nextTaskTimeout});
   }
 
   nextTaskLevel1() {
 
 	this.taskStartTime = performance.now();
+    this.tasksInTest.push(this.task);
 
     this.amountTasks++;
     if (this.testIsEnded) {
@@ -84,9 +91,13 @@ class Test {
     }
 
     this.task.addTaskImage(this.task.image);
+
+    this.setIntervalForNextTask();
   };
 
   nextTaskLevel2(myAudio, btnFlag) {
+    this.taskStartTime = performance.now();
+    this.tasksInTest.push(this.task);
     try {
       this.amountTasks++;
       if (this.testIsEnded) {
@@ -116,6 +127,7 @@ class Test {
     } catch (err) {
       console.log({err});
     }
+    this.setIntervalForNextTask();
   }
 
   checkAnswerProcess(answer) {
@@ -125,16 +137,37 @@ class Test {
 
     if (this.checkAnswer(answer)) {
       this.rightAnswers++;
-      this.passedTasks.push({ task: this.task, answer: 'right', reactionTime });
+      this.passedTasks.push({ answer: 'right', reactionTime });
     } else {
       this.wrongAnswers++;
-      this.passedTasks.push({ task: this.task, answer: 'wrong', reactionTime });
+      this.passedTasks.push({ answer: 'wrong', reactionTime });
     }
 
-    clearInterval(this.nextTaskTimeout);
-    console.log({cleareInterval:this.nextTaskTimeout})
-    this.nextTaskTimeout = setInterval(this.nextTask.bind(this), this.task.AMOUNT_TIME_FOR_TASK_IN_MILLISECONDS);
-    console.log({checkAnswerProcess_createdTimer: this.nextTaskTimeout});
+    if(this.testIsEnded)
+    {
+      if(this.task.audio)
+      {
+        this.task.audio.pause();
+      }
+      $('.leftBtn').delay().fadeOut(500);
+      $('.rightBtn').delay().fadeOut(500);
+      $('#task').delay().fadeOut(500);
+      $('#test_countdown').delay().fadeOut(500);
+      $('#b2').delay(1000).fadeIn(300);
+      $('#reset').delay(1000).fadeIn(300);
+      $('#backToTest').delay(1000).fadeIn(300);
+      $('#nextLevel').delay(1000).fadeIn(300);
+
+      $(".percent").empty();
+
+      $(".percent").append(`Кол-во правильных ответов: ${this.rightAnswers}<br>`);
+      $(".percent").append(`Кол-во ошибочных ответов: ${this.wrongAnswers}<br>`);
+      $(".percent").append(`Кол-во пропущенных заданий: ${this.tasksInTest.length - this.passedTasks.length}<br>`);
+
+      console.log({ passed: this.passedTasks, tasks: this.tasksInTest });
+
+      return;
+    }
     this.nextTask();
   }
 
@@ -162,7 +195,6 @@ class Test {
     this.images = serverResponse.images;
     this.audios = serverResponse.audios;
   };
-
 
   selfRandom(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -213,10 +245,7 @@ class Test {
     $('#backToTest').delay().fadeIn(300);
     $('#nextLevel').delay().fadeIn(300);
   }
-
 }
-
-
 class Task {
   constructor() {
     this.AMOUNT_TIME_FOR_TASK_IN_MILLISECONDS = AMOUNT_TIME_ON_ANSWER_IN_SECONDS * 1000;
@@ -241,7 +270,7 @@ class Task {
       console.log({1: 'playaudio'});
 	  //this.audio.setAttribute('src',fileName);
 	  //this.audio.src = fileName;
-this.audio.load();
+      this.audio.load();
       this.audio.play();
     } catch (err) {
       console.log({err});
@@ -271,7 +300,7 @@ class Timer {
       this.test.renderTestCountdown();
       if (this.test.leastTimeForTest <= 0) {
         clearInterval(countdownInterval);
-        console.log({clearCountDown: countdownInterval});
+        //console.log({"clearCountDown": countdownInterval});
         this.onTestCountdownEnded();
       }
     }, 1000);
@@ -281,31 +310,30 @@ class Timer {
     this.test.testIsEnded = true;
     if (this.test.nextTaskTimeout) {
       clearInterval(this.test.nextTaskTimeout);
-      console.log({this: this, timer: this.test.nextTaskTimeout});
+      console.log({"clearTimeout из окончен тест" : this.test.nextTaskTimeout});
     }
 
-    this.test.task.audio.pause();
-    $('.leftBtn').delay().fadeOut(500);
-    $('.rightBtn').delay().fadeOut(500);
-    $('#task').delay().fadeOut(500);
-    $('#test_countdown').delay().fadeOut(500);
-    $('#b2').delay(1000).fadeIn(300);
-    $('#reset').delay(1000).fadeIn(300);
-    $('#backToTest').delay(1000).fadeIn(300);
-    $('#nextLevel').delay(1000).fadeIn(300);
+    // this.test.task.audio.pause();
+    // $('.leftBtn').delay().fadeOut(500);
+    // $('.rightBtn').delay().fadeOut(500);
+    // $('#task').delay().fadeOut(500);
+    // $('#test_countdown').delay().fadeOut(500);
+    // $('#b2').delay(1000).fadeIn(300);
+    // $('#reset').delay(1000).fadeIn(300);
+    // $('#backToTest').delay(1000).fadeIn(300);
+    // $('#nextLevel').delay(1000).fadeIn(300);
 
-    $(".percent").empty();
 
-    $(".percent").append(`Кол-во правильных ответов: ${this.test.rightAnswers}<br>`);
-    $(".percent").append(`Кол-во ошибочных ответов: ${this.test.wrongAnswers}<br>`);
-
-    console.log({ passed: this.test.passedTasks, tasks: this.test.tasksInTest });
     //$(".percent").append(`Кол-во заданий: ${this.test.amountTasks - 1}`);
   };
 }
 
 class Result {
   constructor(test) {
+    this.skipped = {};
+    this.passed = {};
+    this.amountTasks = {};
+
     this.rightAnswersLevel1 = 0;
     this.wrongAnswersLevel1 = 0;
     this.rightAnswersLevel2_1 = 0;
@@ -327,27 +355,36 @@ class Result {
   setResultForLevel(level, rightAnswers, wrongAnswers, amountTasks, passedTasks, allTasksInLevel) {
     switch (level) {
       case 1: {
-        this.rightAnswersLevel1 = rightAnswers;
-        this.wrongAnswersLevel1 = wrongAnswers;
-        this.amountTasksLevel1 = amountTasks;
-        this.passedTaskLev1 = passedTasks;
-        this.allTasksInLevel1 = allTasksInLevel
+        // this.rightAnswersLevel1 = rightAnswers;
+        // this.wrongAnswersLevel1 = wrongAnswers;
+        // this.amountTasksLevel1 = amountTasks;
+        // this.passedTaskLev1 = passedTasks;
+        // this.allTasksInLevel1 = allTasksInLevel;
+        this.amountTasks["level1"] = allTasksInLevel.length;
+        this.skipped["level1"] = allTasksInLevel.length - passedTasks.length;
+        this.passed["level1"] = passedTasks;
         break;
       }
       case 2: {
-        this.rightAnswersLevel2_1 = rightAnswers;
-        this.wrongAnswersLevel2_1 = wrongAnswers;
-        this.amountTasksLevel2_1 = amountTasks;
-        this.passedTaskLev2_1 = passedTasks;
-        this.allTasksInLevel2_1 = allTasksInLevel
+        // this.rightAnswersLevel2_1 = rightAnswers;
+        // this.wrongAnswersLevel2_1 = wrongAnswers;
+        // this.amountTasksLevel2_1 = amountTasks;
+        // this.passedTaskLev2_1 = passedTasks;
+        // this.allTasksInLevel2_1 = allTasksInLevel;
+        this.amountTasks["level2"] = allTasksInLevel.length;
+        this.skipped["level2"] = allTasksInLevel.length - passedTasks.length;
+        this.passed["level2"] = passedTasks;
         break;
       }
       case 3: {
-        this.rightAnswersLevel2_2 = rightAnswers;
-        this.wrongAnswersLevel2_2 = wrongAnswers;
-        this.amountTasksLevel2_2 = amountTasks;
-        this.passedTaskLev2_2 = passedTasks;
-        this.allTasksInLevel2_2 = allTasksInLevel
+        // this.rightAnswersLevel2_2 = rightAnswers;
+        // this.wrongAnswersLevel2_2 = wrongAnswers;
+        // this.amountTasksLevel2_2 = amountTasks;
+        // this.passedTaskLev2_2 = passedTasks;
+        // this.allTasksInLevel2_2 = allTasksInLevel;
+        this.amountTasks["level3"] = allTasksInLevel.length;
+        this.skipped["level3"] = allTasksInLevel.length - passedTasks.length;
+        this.passed["level3"] = passedTasks;
         break;
       }
     }
@@ -376,25 +413,20 @@ $(document).ready(function () {
   $('#next').on('click', function () {
     hideAndShowElements();
 
-    var test = new Test({leftbutton: '#leftbutton'});
-
+    var test = new Test();
+    initEventListeners();
     var result = new Result(test);
     test.infoAboutLevel1();
 
     function initEventListeners() {
       document.addEventListener("keydown", onKeyDown);
-      leftButton.on('click', checkLeftButtonAnswer);
-      rightButton.on('click', checkRightButtonAnswer);
+      leftButton.on('click', checkButtonAnswer);
+      rightButton.on('click', checkButtonAnswer);
     }
 
-    function checkLeftButtonAnswer() {
+    function checkButtonAnswer() {
       test.checkAnswerProcess(this.name);
     }
-
-    function checkRightButtonAnswer() {
-      test.checkAnswerProcess(this.name);
-    }
-
     function onKeyDown(e) {
       switch (e.keyCode) {
         case KEYCODE_LEFT_BUTTON: {
@@ -426,9 +458,9 @@ $(document).ready(function () {
           clearInterval(firstTimerCountdown);
           test.nextTask();
           test.timer = new Timer(test);
-          test.setIntervalForNextTask();
-			test.reaction = performance.now();
-			console.log(test.reaction);
+          //test.setIntervalForNextTask();
+          test.reaction = performance.now();
+          console.log(test.reaction);
         }
         console.log(seconds);
         $('#start_counter').html(seconds);
@@ -452,7 +484,8 @@ $(document).ready(function () {
 
       result.setResultForLevel(test.level, test.rightAnswers, test.wrongAnswers, test.amountTasks, test.passedTasks, test.tasksInTest);
       let newLevel = test.level + 1;
-      test = new Test();
+
+      test.newLevel();
       test.setLevel(newLevel);
 
       switch (test.level) {
@@ -472,39 +505,87 @@ $(document).ready(function () {
 
 	function CalcPercentRightAnswers(result)
 	{
-		amountRightAnswersLevel1 = result.rightAnswersLevel1;
-          amountRightAnswersLevel2 = result.rightAnswersLevel2_1;
-          amountRightAnswersLevel3 = result.rightAnswersLevel2_2;
-          amountWrongAnswersLevel1 = result.wrongAnswersLevel1;
-          amountWrongAnswersLevel2 = result.wrongAnswersLevel2_1;
-          amountWrongAnswersLevel3 = result.wrongAnswersLevel2_2;
+		//   amountRightAnswersLevel1 = result.rightAnswersLevel1;
+        //   amountRightAnswersLevel2 = result.rightAnswersLevel2_1;
+        //   amountRightAnswersLevel3 = result.rightAnswersLevel2_2;
+        //   amountWrongAnswersLevel1 = result.wrongAnswersLevel1;
+        //   amountWrongAnswersLevel2 = result.wrongAnswersLevel2_1;
+        //   amountWrongAnswersLevel3 = result.wrongAnswersLevel2_2;
+        //
+		// const amountTasks1 = result.amountTasksLevel1 + result.amountTasksLevel2_1 + result.amountTasksLevel2_2;
+		//
+		// const amountTasks = result.amountTasks.level1 + result.amountTasksLevel2_1 + result.amountTasksLevel2_2;
+		//
+		// const allRight =amountRightAnswersLevel1 + amountRightAnswersLevel2 + amountRightAnswersLevel3;
+		// const allWrong =amountWrongAnswersLevel1+amountWrongAnswersLevel2+amountWrongAnswersLevel3;
 
-		const amountTasks = result.amountTasksLevel1 + result.amountTasksLevel2_1 + result.amountTasksLevel2_2;
-		const allRight =amountRightAnswersLevel1 + amountRightAnswersLevel2 + amountRightAnswersLevel3;
-		const allWrong =amountWrongAnswersLevel1+amountWrongAnswersLevel2+amountWrongAnswersLevel3;
+		const percentRightAnswers1 = String(Math.ceil(result.passed.level1.filter((x) => x.answer == "right").length * 100 / result.amountTasks.level1).toPrecision(2))
+		const percentRightAnswers2 = String(Math.ceil(result.passed.level2.filter((x) => x.answer == "right").length * 100 / result.amountTasks.level2).toPrecision(2))
+		const percentRightAnswers3 = String(Math.ceil(result.passed.level3.filter((x) => x.answer == "right").length * 100 / result.amountTasks.level3).toPrecision(2))
 
-		return String(Math.ceil(allRight * 100 / amountTasks).toPrecision(2));
+		return [percentRightAnswers1, percentRightAnswers2, percentRightAnswers3];
 	}
     function SendResult(result)
     {
-		$.ajax({
-			async: false,
-			url: "result.php",
-			type: "POST",
-			data: {
-				amountRightAnswersLevel1: result.rightAnswersLevel1,
-			  amountRightAnswersLevel2: result.rightAnswersLevel2_1,
-			  amountRightAnswersLevel3: result.rightAnswersLevel2_2,
-			  amountWrongAnswersLevel1: result.wrongAnswersLevel1,
-			  amountWrongAnswersLevel2: result.wrongAnswersLevel2_1,
-			  amountWrongAnswersLevel3: result.wrongAnswersLevel2_2,
-			  timeForTest: result.test.AMOUNT_TIME_FOR_TEST_IN_SECONDS,
-			  timeForTask: result.test.task.AMOUNT_TIME_FOR_TASK_IN_MILLISECONDS / 1000
-			}
+      const rightAnswers1 = result.passed.level1.filter((x) => x.answer == "right");
+      const wrongAnswers1 = result.passed.level1.filter((x) => x.answer == "wrong");
+
+      const reactionsRight1 = getAverage(rightAnswers1, rightAnswers1.length);
+      const reactionsWrong1 = getAverage(wrongAnswers1, wrongAnswers1.length);
+
+      const rightAnswers2 = result.passed.level2.filter((x) => x.answer == "right");
+      const wrongAnswers2 = result.passed.level2.filter((x) => x.answer == "wrong");
+
+      const reactionsRight2 = getAverage(rightAnswers2, rightAnswers2.length);
+      const reactionsWrong2 = getAverage(wrongAnswers2, wrongAnswers2.length);
+
+      const rightAnswers3 = result.passed.level3.filter((x) => x.answer == "right");
+      const wrongAnswers3 = result.passed.level3.filter((x) => x.answer == "wrong");
+
+      const reactionsRight3 = getAverage(rightAnswers3, rightAnswers3.length);
+      const reactionsWrong3 = getAverage(wrongAnswers3, wrongAnswers3.length);
+
+
+      const reactionsWrong = result.passed.level1.filter((x) => x.answer == "wrong").reactionTime;
+      $.ajax({
+        async: false,
+        url: "result.php",
+        type: "POST",
+        data: {
+          amountRightAnswersLevel1: result.passed.level1.filter((x) => x.answer == "right").length,
+          amountRightAnswersLevel2: result.passed.level2.filter((x) => x.answer == "right").length,
+          amountRightAnswersLevel3: result.passed.level3.filter((x) => x.answer == "right").length,
+          amountWrongAnswersLevel1: result.passed.level1.filter((x) => x.answer == "wrong").length,
+          amountWrongAnswersLevel2: result.passed.level2.filter((x) => x.answer == "wrong").length,
+          amountWrongAnswersLevel3: result.passed.level3.filter((x) => x.answer == "wrong").length,
+          timeForTest: result.test.AMOUNT_TIME_FOR_TEST_IN_SECONDS,
+          timeForTask: result.test.task.AMOUNT_TIME_FOR_TASK_IN_MILLISECONDS / 1000,
+          passed: result.passed,
+          amountTasks: result.amountTasks,
+          skipped: result.skipped,
+          reactionsRight1: reactionsRight1 / 1000,
+          reactionsRight2: reactionsRight2 / 1000,
+          reactionsRight3: reactionsRight3 / 1000,
+          reactionsWrong1: reactionsWrong1 / 1000,
+          reactionsWrong2: reactionsWrong2 / 1000,
+          reactionsWrong3: reactionsWrong3 / 1000,
+        }
       })
     }
+    function getAverage(obj, count)
+    {
+      if(obj.lenght === 0)
+      {
+        return 0;
+      }
+      const times = obj.map(function (x) {
+        return x.reactionTime;
+      });
 
-    //оказать рабочую область - задания, кнопки выбора, таймер
+      return times.reduce((a,b) => Number(a)+Number(b)) / count;
+    }
+
+    //показать рабочую область - задания, кнопки выбора, таймер
     function ShowWorkPlace() {
       $('.leftBtn').delay(4000).fadeIn();
       $('.rightBtn').delay(4000).fadeIn();
@@ -520,13 +601,8 @@ $(document).ready(function () {
 
       hideInstruction();
 
-      initEventListeners();
-
-      // TODO: refactor this function
-
       firstTimer();
-      $('#start_counter').delay(4000).fadeOut();
-      $('#start_counter').empty();
+      hideAndClearStartTimer();
       ShowWorkPlace();
     };
 
@@ -534,6 +610,12 @@ $(document).ready(function () {
     {
       $('#infoAboutLevel').delay().fadeOut();
       $('#go').delay().fadeOut();
+    }
+
+    function hideAndClearStartTimer()
+    {
+      $('#start_counter').delay(4000).fadeOut();
+      $('#start_counter').empty();
     }
   });
 
@@ -577,6 +659,9 @@ function addResultsToCommonTable(result){
 		type: "POST", 
 		data: 
 		 { 
+			right1:result[0],
+			right2:result[1],
+			right3:result[2],
 			result:result,
 			version: "site version"
 		 }
